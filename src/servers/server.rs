39ea9198ca9d61814724318d7738937f23564e15
Javier <javier7570@gmail.com>
2021-05-05 09:29:45 +0100

    Fix some errors

--- src/servers/server.rs
+++ src/servers/server.rs
@@ -22,101 +22,98 @@ struct ServerData {
    nodes: Vec<Node>
 }
 
-fn start_server(server_data: &ServerData) -> Result<(), Box<dyn Error>> {
-   let mut poll = Poll::new()?;
-   let mut events = Events::with_capacity(128);
-   let mut clients: HashMap<usize, TcpStream> = HashMap::new();
+impl ServerData {
+   fn start(&mut self) -> Result<(), Box<dyn Error>> {
+      let mut poll = Poll::new()?;
+      let mut events = Events::with_capacity(128);
+      let mut clients: HashMap<usize, TcpStream> = HashMap::new();
  
-   let mut server = TcpListener::bind(server_data.addr)?;
-   poll.registry().register(&mut server, SERVER, Interest::READABLE)?;
+      let mut server = TcpListener::bind(self.addr)?;
+      poll.registry().register(&mut server, SERVER, Interest::READABLE)?;
 
-   let mut token_counter: usize = 0;
+      let mut token_counter: usize = 0;
 
-   loop {
-      poll.poll(&mut events, None)?;
+      loop {
+         poll.poll(&mut events, None)?;
 
-      for event in events.iter() {
-         match event.token() {
-            SERVER => {
-               if event.is_error() {
-                  match server.take_error() {
-                     Ok(Some(e)) | Err(e) => return Err(Box::new(e)),
-                     Ok(None) => panic!("Unknown error")
+         for event in events.iter() {
+            match event.token() {
+               SERVER => {
+                  if event.is_error() {
+                     match server.take_error() {
+                        Ok(Some(e)) | Err(e) => return Err(Box::new(e)),
+                        Ok(None) => panic!("Unknown error")
+                     }
+                  }
+
+                  match server.accept() {
+                     Ok((mut client, _)) => {
+                        token_counter += 1;
+                        poll.registry().register(&mut client, Token(token_counter), Interest::READABLE )?;
+                        clients.insert(token_counter, client);
+                     }
+                     Err(ref e) if e.kind() == ErrorKind::WouldBlock =>
+                        break,
+                     Err(e) => 
+                        panic!("Unexpected error: {}", e)
                   }
                }
 
-               match server.accept() {
-                  Ok((mut client, _)) => {
-                     token_counter += 1;
-                     poll.registry().register(&mut client, Token(token_counter), Interest::READABLE )?;
-                     clients.insert(token_counter, client);
+               Token(id) => {
+                  let mut client = clients.get(&id).unwrap();
+                  if event.is_error() {
+                     match client.take_error() {
+                        Ok(Some(e)) | Err(e) => return Err(Box::new(e)),
+                        Ok(None) => panic!("Unknown error")
+                     }
                   }
-                  Err(ref e) if e.kind() == ErrorKind::WouldBlock =>
-                     break,
-                  Err(e) => 
-                     panic!("Unexpected error: {}", e)
+
+                  if event.is_writable() {
+                  }
+
+                  if event.is_readable() {
+                     self.handle_rq(&mut client);
+                  }
+                  return Ok(());
                }
             }
+        }
+      }
+   }
 
-            Token(id) => {
-               let client = clients.get(&id).unwrap();
-               if event.is_error() {
-                  match client.take_error() {
-                     Ok(Some(e)) | Err(e) => return Err(Box::new(e)),
-                     Ok(None) => panic!("Unknown error")
-                  }
-               }
-
-               if event.is_writable() {
-               }
-
-               if event.is_readable() {
-                  handle_rq(&mut client);
-               }
-               return Ok(());
+   fn send_message(&mut self, message: &[u8]) -> bool {
+      for s in &mut self.nodes {
+         if s.stream.is_none() {
+            if let Ok(stream) = TcpStream::connect(s.addr) {
+               s.stream = Some(stream)
             }
-
-            // We don't expect any events with tokens other than those we provided.
-             _ => unreachable!(),
+            else {
+               println!("{:#?}", s.addr);
+               continue;
+            }
          }
-     }
+         let remote: &mut TcpStream = s.stream.as_mut().unwrap();
+         remote.write(message).unwrap();
+      }
+      return true;
    }
 
-   Ok(())
-}
-
-fn send_message(mut stream: &TcpStream, message: &[u8]) -> bool {
-   for s in &mut nodes {
-      if s.stream.is_none() {
-         if let Ok(stream) = TcpStream::connect(s.addr) {
-            s.stream = Some(stream)
+   fn handle_rq(&mut self, mut stream: &TcpStream) {
+      let mut data = [0 as u8; 50]; // using 50 byte buffer
+      while match stream.read(&mut data) {
+         Ok(size) => {
+            // echo everything!
+            println!("Received message");
+            self.send_message(&data[0..size]);         
+            true
+         },
+         Err(_) => {
+            println!("An error occurred, terminating connection with {}", stream.peer_addr().unwrap());
+            stream.shutdown(Shutdown::Both).unwrap();
+            false
          }
-         else {
-            println!("{:#?}", s.addr);
-            continue;
-         }
-      }
-      let remote: &mut TcpStream = s.stream.as_mut().unwrap();
-      remote.write(message).unwrap();
+      } {}
    }
-   return true;
-}
-
-fn handle_rq(mut stream: &TcpStream) {
-   let mut data = [0 as u8; 50]; // using 50 byte buffer
-   while match stream.read(&mut data) {
-      Ok(size) => {
-         // echo everything!
-         println!("Received message");
-         send_message(&data[0..size]);         
-         true
-      },
-      Err(_) => {
-         println!("An error occurred, terminating connection with {}", stream.peer_addr().unwrap());
-         stream.shutdown(Shutdown::Both).unwrap();
-         false
-      }
-   } {}
 }
 
 pub fn create_server(config_lock: Arc<RwLock<Config>>) {
@@ -126,8 +123,8 @@ pub fn create_server(config_lock: Arc<RwLock<Config>>) {
             .map(|(_, v)| Node {addr: v.clone(), stream: None})
             .collect();
 
-   let server = ServerData{ addr: SocketAddr::from(([0, 0, 0, 0], config.port)),
+   let mut server = ServerData{ addr: SocketAddr::from(([0, 0, 0, 0], config.port)),
                             nodes: nodes };
    
-   start_server(&server);  
+   server.start().unwrap();  
 }
